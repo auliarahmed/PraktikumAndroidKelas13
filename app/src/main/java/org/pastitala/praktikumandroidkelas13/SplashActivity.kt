33635078020d5e5dc26dd2895aaebbf2df0b2d5f
Rahmat Aulia <auliarahmed@gmail.com>
2020-12-25 00:07:10 +0800

    selesai praktikum ke 2 penambahan splashscreen kotlin hide actionbar

---
+++ app/src/main/java/org/pastitala/praktikumandroidkelas13/SplashActivity.kt
@@ -0,0 +1,21 @@
+package org.pastitala.praktikumandroidkelas13
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }, 2000)
+
+    }
+}
